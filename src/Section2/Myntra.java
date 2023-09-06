e9d09a0ba43896d2433020ae2d4e4597fa69c699
naresh7770 <98827254+naresh7770@users.noreply.github.com>
2023-09-06 11:14:34 +0530

    4

--- src/Section2/Myntra.java
+++ src/Section2/Myntra.java
@@ -3,10 +3,11 @@ package Section2;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Myntra {
-
-	public static void main(String[] args) {
+@Test
+	public void TestMyn() {
 		System.setProperty("Webdriver.chrome.driver", "C:\\Users\\Kandagatla Naresh\\eclipse-workspace\\Automation\\src");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.myntra.com/");
